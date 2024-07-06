0f28afc1a8e1fab4adf816daedbabd6b584b1846
abdulmominsakib <mdmomin322@gmail.com>
2024-07-06 15:08:37 +0600

    Updated to latest SDK

--- android/app/src/main/kotlin/com/abdulmomin/groceries_flutter_ui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.abdulmomin.groceries_flutter_ui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/groceries_flutter_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.groceries_flutter_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
